f653329e3dc80eba30407ee435fbd10095ca5f88
James Daniel Farrell <j.d.farrell00@gmail.com>
2021-02-28 11:02:32 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='vibrational_perturbation_theory',
+    version='0.1.0',
+    packages=['pyorca', 'pyvpt2'],
+    url='',
+    license='MIT',
+    author='James Farrell',
+    author_email='farrelljd@iphy.ac.cn',
+    description=('An implementation of Barone\'s generalized second-order vibrational perturbation theory,'
+                 ' packaged with a wrapper for the freely-available quantum chemistry package ORCA.')
+)
